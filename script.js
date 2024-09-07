75d9f6cd177bcf7a0b15c46024a6ef9a4f5e7ecc
Akashx2606 <137022133+Akashx2606@users.noreply.github.com>
2024-09-07 10:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+const sortedBands = bands.sort((a, b) => strip(a) > 
+	strip(b) ? 1 : -1);
 
+document.querySelector('#bands').innerHTML = 
+	sortedBands.map(band => `<li>${band}</li>`).join('');
